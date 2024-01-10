d15c9ff021f96d9003867fa8301a77bf7582c448
Tobias Høegh <tobias@tujo.no>
2024-01-10 15:52:25 +0100

    chore: remove labelDescription, labelSecondary and characterCounter

--- packages/dnb-eufemia/src/extensions/forms/Field/Expiry/Expiry.tsx
+++ packages/dnb-eufemia/src/extensions/forms/Field/Expiry/Expiry.tsx
@@ -29,8 +29,6 @@ function Expiry(props: ExpiryProps) {
     help,
     disabled,
     value = '',
-    labelDescription,
-    labelSecondary,
     layout = 'vertical',
     required,
     handleFocus,
@@ -53,8 +51,6 @@ function Expiry(props: ExpiryProps) {
   return (
     <FieldBlock
       className={classnames('dnb-forms-field-expiry', className)}
-      labelSecondary={labelSecondary}
-      labelDescription={labelDescription}
       info={info}
       warning={warning}
       error={error}
